a4e6ec02d1ae3f45236f357d6f0f6be5ad4e78a0
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-11 00:38:16 +0700

    Add files via upload

---
+++ program operator penugasab.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int a = 5;
+	int b = 7;
+	int c = 4;
+	int d = 8;
+	int e = 9;
+
+	a+=10; // a = a + 10
+	
+	b-=12; // b = b - 12
+	
+	c*=18; // c = c * 18
+	
+	d/=4;  // d = d / 4
+	
+	e%=5;  // e = e % 5
+	
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	 
+	return 0;
+}
