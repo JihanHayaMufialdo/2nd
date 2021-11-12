4be9a0b1d3b3a88bc69a0cb36e22c2389f9153da
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-12 23:09:40 +0700

    LUAS PERMUKAAN & VOLUME BANGUN RUANG

--- L.P. & Volume Tabung.cpp
+++ L.P. & Volume Tabung.cpp
@@ -1,34 +1,16 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main (){
-//	float r,t;
-//	float phi = 3.14;
-//	
-//	cout << "\n------- TABUNG -------\n\n";
-//	cout << "jari-jari (m) : ";
-//	cin  >> r; 
-//	cout << "tinggi (m)    : ";
-//	cin  >> t;
-//	
-//	cout << "\nLuas Permukaan  : " << phi*r*r*t << " m^2\n";
-//	cout << "Volume\t\t: " << 2*phi*r*(r+t) << " m^3";
-//}
 #include <iostream>
 using namespace std;
 
 int main (){
-	float r,t,s;
+	float r,t;
 	float phi = 3.14;
-	
-	cout << "\n------- KERUCUT -------\n\n";
-	cout << "jari-jari (m)   : ";
+
+	cout << "\n------- TABUNG -------\n\n";
+	cout << "jari-jari (m) : ";
 	cin  >> r; 
-	cout << "tinggi (m)      : ";
-	cin  >> t; 
-	cout << "sisi miring (m) : ";
-	cin  >> s;
+	cout << "tinggi (m)    : ";
+	cin  >> t;
 	
-	cout << "\nLuas Permukaan  : " << phi*r*(s+r) << " m^2\n";
-	cout << "Volume\t\t: " << 1/3*phi*r*r*t << " m^3";
+	cout << "\nLuas Permukaan  : " << phi*r*r*t << " m^2";
+	cout << "\nVolume\t\t: " << 2*phi*r*(r+t) << " m^3";
 }
