09f2e6a4af9b38a9c24410cdaf132644ad8e9950
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-12 23:04:57 +0700

    Add files via upload

---
+++ L.P. & Volume Kubus.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	float s;
+	
+	cout << "\n------- KUBUS -------\n\n";
+	cout << "sisi (m)       : ";
+	cin  >> s; 
+	
+	cout << "\nLuas Permukaan  : " << 6*(s*s)<< " m\n";
+	cout << "Volume\t\t: " << s*s*s << " m^2";
+
+}
