591daa833e23964239e3d075512afee9b1dfbe54
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-12 23:04:39 +0700

    Add files via upload

---
+++ L.P. & Volume Kerucut.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	float r,t,s;
+	float phi = 3.14;
+	
+	cout << "\n------- KERUCUT -------\n\n";
+	cout << "jari-jari (m)   : ";
+	cin  >> r; 
+	cout << "tinggi (m)      : ";
+	cin  >> t; 
+	cout << "sisi miring (m) : ";
+	cin  >> s;
+	
+	cout << "\nLuas Permukaan  : " << phi*r*(s+r) << " m^2\n";
+	cout << "Volume\t\t: " << (phi*r*r*t)/3 << " m^3";
+}
