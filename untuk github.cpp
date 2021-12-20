124b8eac40836a82d11b56f786404c9f6a0ed94f
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-20 22:23:46 +0700

    Update untuk github.cpp

--- untuk github.cpp
+++ untuk github.cpp
@@ -5,11 +5,12 @@ int main(){
 
 	int x;
 	
-	cout << "f(x) = 2x^3 + 5x^2 - 7x +10\n\n";
+	cout << "f(x) = 2x^3 + 5x^2 - 7x + 10\n\n";
 	
 	cout << "x = ";
 	cin >> x;
 	
 	cout << "\nf(" << x << ") = " << 2*(x*x*x) + 5*(x*x) - 7*x +10;
 	
+	return 0;
 }
