9e699d86030d7409eb8a44ea6f01162ff7e726f5
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-21 23:31:39 +0700

    Update untuk github.cpp

--- untuk github.cpp
+++ untuk github.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main(){
+	
+	float r,t;
+	float phi = 3.14;
 
-	int r,t;
+	cout << "r = ";
+	cin  >> r; 
+	cout << "t = ";
+	cin  >> t;
 	
-	
+	cout << "\nLuas Permukaan  : " << phi*r*r*t << " m^2";
+	cout << "\nVolume\t\t: " << 2*phi*r*(r+t) << " m^3";
 	
 }
